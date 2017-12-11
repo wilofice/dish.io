597ede4caeaf68b518d5654a2019d57b77d2fa32
wilofice <wilofice@gmail.com>
2017-12-11 15:18:42 +0000

    Adding Interface UserModel

---
+++ src/models/restaurant/restaurant.model.ts
@@ -0,0 +1,9 @@
+export interface Restaurant {
+	key?: string;
+	username: string;
+	name: string;
+	email: string;
+	password: string;
+	tel: string;
+	address: string;
+}
---
+++ src/models/user/user.model.ts
@@ -0,0 +1,11 @@
+export interface UserModel {
+    key?: string;
+    username: string;
+    nom: string;
+    prenom: string;
+	email: string;
+    password: string;
+    tel: string;
+    address: string;
+    type: string;
+}
